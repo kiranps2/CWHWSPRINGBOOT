0ce8b01d060e0e15ad4ee7065a43ebb39499e629
kiran ps <kiranpsofficial1@gmail.com>
2026-09-11 09:54:56 +0530

    Refactor CWDAY3 and HWDAY3 controllers; add day3hw.html template for movie description

--- src/main/java/com/example/CWHWSPRINGBOOT/Controller/CWDAY3.java
+++ src/main/java/com/example/CWHWSPRINGBOOT/Controller/CWDAY3.java
@@ -1,18 +1,19 @@
 package com.example.CWHWSPRINGBOOT.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 @Controller
 public class CWDAY3 {
-	@GetMapping("/home")
-    public String home(Model model) {
-        String message = "The beautiful thing about learning is that no one can take it away from you.";
+	@GetMapping("/cw3")
+    public String cw3(Model model) {
+        String message = "Hello from Spring Boot!";
         model.addAttribute("message", message);
         String hello = "<h1>Hello, World!</h1>";
         model.addAttribute("hello", hello);
         boolean isLoggedIn = true;
         model.addAttribute("isLoggedIn", isLoggedIn);
-        return "home";
+        return "day3";
     }
 }
 
--- src/main/java/com/example/CWHWSPRINGBOOT/Controller/HWDAY3.java
+++ src/main/java/com/example/CWHWSPRINGBOOT/Controller/HWDAY3.java
@@ -1,15 +1,20 @@
 package com.example.CWHWSPRINGBOOT.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+@Controller
 public class HWDAY3 {
-	public String home(Model model) {
-        String message = "The beautiful thing about learning is that no one can take it away from you.";
-        model.addAttribute("message", message);
-        String hello = "<h1>Hello, World!</h1>";
-        model.addAttribute("hello", hello);
-        boolean isLoggedIn = true;
+	@GetMapping("/hw3")
+	public String hw3(Model model) {
+        String movie = "godzilla";
+        model.addAttribute("message", movie);
+        String discription = "Godzilla is a gigantic prehistoric creature and one of the most famous monsters in popular culture. Often portrayed as a massive, dinosaur-like reptile, Godzilla is known for his enormous size, incredible strength, tough, nearly indestructible body, and devastating atomic breath.";
+        model.addAttribute("discription", discription);
+        boolean isLoggedIn = false;
         model.addAttribute("isLoggedIn", isLoggedIn);
-        return "home";
+        return "day3hw";
     }
 }
