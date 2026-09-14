0153ff3d337e64973650ae5b7306c9b8f8964723
kiran ps <kiranpsofficial1@gmail.com>
2026-09-14 10:39:43 +0530

    Add CWDAY5 controller and templates for index and product pages; update CWDAY3 message

--- src/main/java/com/example/CWHWSPRINGBOOT/Controller/CWDAY3.java
+++ src/main/java/com/example/CWHWSPRINGBOOT/Controller/CWDAY3.java
@@ -1,13 +1,14 @@
 package com.example.CWHWSPRINGBOOT.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 @Controller
 public class CWDAY3 {
 	@GetMapping("/cw3")
     public String cw3(Model model) {
-        String message = "Hello from Spring Boot!";
+        String message = "welcome to my app!";
         model.addAttribute("message", message);
         String hello = "<h1>Hello, World!</h1>";
         model.addAttribute("hello", hello);
@@ -15,5 +16,7 @@ public class CWDAY3 {
         model.addAttribute("isLoggedIn", isLoggedIn);
         return "day3";
     }
+	
+	
 }
 
---
+++ src/main/java/com/example/CWHWSPRINGBOOT/Controller/CWDAY5.java
@@ -0,0 +1,29 @@
+package com.example.CWHWSPRINGBOOT.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+@Controller
+public class CWDAY5 {
+		@GetMapping("/index")
+		public String message(Model model) {
+			 String message = "Hello from Spring Boot!";
+		        model.addAttribute("message", message);
+		        return "day5CWindex" ;
+		}
+//		@GetMapping("/product")
+//		public String product() {
+//			return "day5cwproduct" ;
+//		}
+		@GetMapping("/product")
+		public String promessage(Model model) {
+			 String promessage = "welcome to my product page";
+		        model.addAttribute("promessage", promessage);
+		        return "day5CWproduct" ;
+		}
+//	    @GetMapping("/partials")
+//	    public String headfooter() {
+//	        return "day5CWindex";
+//	    }
+}
