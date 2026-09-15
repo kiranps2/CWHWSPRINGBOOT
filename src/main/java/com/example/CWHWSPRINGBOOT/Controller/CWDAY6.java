163a46aa19e86a8cd5c5aca7d4da1f7abe94788b
kiran ps <kiranpsofficial1@gmail.com>
2026-09-15 10:32:01 +0530

    Add CWDAY6 controller and templates for form submission; implement username handling and welcome page

---
+++ src/main/java/com/example/CWHWSPRINGBOOT/Controller/CWDAY6.java
@@ -0,0 +1,20 @@
+package com.example.CWHWSPRINGBOOT.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class CWDAY6 {
+	@GetMapping("/form")
+	public String processform() {
+		
+		return "username";
+	}
+	
+	
+	@GetMapping("/welcome")
+	public String processform(@RequestParam(required=false) String username,Model model) {
+		model.addAttribute("username",username);
+		return "welcome";
+	}
+}
